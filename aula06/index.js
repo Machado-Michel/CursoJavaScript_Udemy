b5285ce9f5592dfd71ced10712140d28941b1653
Michel Machado <mr2710michel@gmail.com>
2024-02-21 14:30:39 -0300

    Feito Aula 06

---
+++ aula06/index.js
@@ -0,0 +1,33 @@
+//Não podemos criar palavras com palavras reservadas.
+//Variáveis precisam ter nomes significativos.
+// Não pode começar o nome de uma variável com um número.
+// Não podem conter espaços ou traços
+
+// Utilizamos camelCase
+let nomeCompletoDoCliente = 'Michel dos Reis Machado';
+
+//Case-Sensitive 
+// NÃO UTILIZE VAR, UTILIZE LET.
+
+
+/*------------------------------------
+
+let nome; // Declarou a variável
+nome = 'João'; //Inicializando a Variável
+console.log(nome);
+nome = 'Mijas';
+console.log(nome);
+------------------------------------*/
+
+
+/* ------------------------------------
+
+let nome = 'Mijas'; //String
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000,', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com', nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo.'); 
+
+------------------------------------*/
