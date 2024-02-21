e49d5361efc6210432f6e789b138be319834a1cc
Michel Machado <mr2710michel@gmail.com>
2024-02-21 19:30:49 -0300

    Assistido aula 10

---
+++ aula10/index.js
@@ -0,0 +1,10 @@
+const nome = 'Michel'; // String
+const nome1 = "Michel"; // String
+const nome2 = `Michel`; // String
+const num1 = 10; // Number
+const num2 = 10.52; // Number
+let nomeAluno; // Undefined = Não aponta para nenhum Local na Memória do Motor JS.
+let sobrenomeAluno = null; // Nulo = Não aponta para nenhum Local na Memória do Motor JS.
+const aprovado = false; // Boolean = True ou False. (Lógico)
+
+console.log(typeof sobrenomeAluno, sobrenomeAluno)
