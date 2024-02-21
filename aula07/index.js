afa10fae173d380ad9056ebf7bdf83ed9d91a78c
Michel Machado <mr2710michel@gmail.com>
2024-02-21 14:58:31 -0300

    Assistido Aula 07

---
+++ aula07/index.js
@@ -0,0 +1,20 @@
+//Não podemos criar palavras com palavras reservadas.
+//Variáveis precisam ter nomes significativos.
+// Não pode começar o nome de uma variável com um número.
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+//Case-Sensitive
+// Não pode modificar o valor de uma constante.
+// NÃO UTILIZE VAR, UTILIZE CONST.
+
+// + - * /
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 247;
+
+
+console.log(typeof(primeiroNumero))
