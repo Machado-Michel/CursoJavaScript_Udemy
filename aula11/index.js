2721e64422e59019afa0eee0b73fb499aec5785e
Michel Machado <mr2710michel@gmail.com>
2024-02-21 20:37:34 -0300

    Assistido aula 11

---
+++ aula11/index.js
@@ -0,0 +1,37 @@
+/*
+*** Operadores Aritiméticos.
++ Adição e Concatenação
+- Subtração
+/ Divisão
+* Multiplicação
+** Potenciação
+% Resto da Divisão
+*/
+
+/*
+Precedencia Aritimética
+
+()
+**
+* / %
++  -
+
+*/
+
+/*
+const num1 = 5;
+const num2 = 2;
+const num3 = 17;
+console.log((num1 * num2 / num3).toFixed(2));
+console.log('-------------------Proxima Parte-------------------');*/
+
+//----------------------------------------------------------------------------
+
+let contador = 2;
+contador **= 10.5;
+console.log((contador).toFixed(2))
+console.log('-------------------Proxima Parte-------------------');
+
+const num1 = 10;
+const num2 = Number('5');
+console.log(num1 + num2)
