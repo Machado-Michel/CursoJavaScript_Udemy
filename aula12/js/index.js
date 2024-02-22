dfb4e6a64fdcc2c640c5c35aabbd3843cc0b4a2c
Michel Machado <mr2710michel@gmail.com>
2024-02-21 21:25:58 -0300

    Assistido e Feito exercicios da Aula 12

---
+++ aula12/js/index.js
@@ -0,0 +1,5 @@
+let num1 = window.prompt('Digite o primeiro valor:');
+let num2 = window.prompt('Digite o segundo valor:');
+num1 = Number(num1);
+num2 = Number(num2);
+window.alert(`O resultado da soma foi de ${num1 + num2}.`);
