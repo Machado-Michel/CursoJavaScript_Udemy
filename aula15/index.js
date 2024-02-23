96fffca75642f1c24fc23240a38daec77ed25b69
Michel Machado <mr2710michel@gmail.com>
2024-02-22 21:36:09 -0300

    Feito aula 15

---
+++ aula15/index.js
@@ -0,0 +1,8 @@
+console.log(100 / 0);
+
+//let num2 = Math.floor(num1); // ----------- || Arredondou o valor para baixo com Math.floor(num1)
+//let num2 = Math.ceil(num1); // ----------- || Arredondou o valor para cima com Math.ceil(num1)
+//let num2 = Math.round(num1) // ----------- || Arredonda o valor para cima ou para baixo com Math.round(num1)
+//console.log(Math.max(1,2,3,4,5,-10,-48,-152,1005,125,2017,2048));
+//console.log(Math.min(1,2,3,4,5,-10,-48,-152,1005,125,2017,2048));
+//const aleatorio = Math.random().toFixed(2) * (10 - 5) + 5;
