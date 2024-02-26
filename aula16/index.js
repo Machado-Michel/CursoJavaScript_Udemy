7410249d171bac6739118222bd21ed941dd5221b
Michel Machado <mr2710michel@gmail.com>
2024-02-26 16:07:28 -0300

    Feito aula 16

---
+++ aula16/index.js
@@ -0,0 +1,27 @@
+const alunos = ['Luiz', 'Maria', 'João'];
+
+console.log( typeof alunos);
+console.log (alunos instanceof Array);
+
+// alunos.push('Mijas');
+// alunos.push('Jesca')
+
+// delete alunos[1]; // Deleta o indece do array selecionado
+
+// const removido = alunos.shift(); // Remove do Inicio do Array
+// const removido = alunos.pop(); // Remove do Final do Array
+
+//alunos.unshift('Mijinhas'); // Adiciona no Inicio do Array
+//alunos.unshift('Chernow');
+
+//alunos.push('Jorge', 'Michel', 'Anja'); // Adiciona no Fim do Array
+
+
+//alunos[alunos.length] = 'Luiza';
+//alunos[alunos.length] = 'Fábio';
+//alunos[alunos.length] = 'Mijas';
+
+//console.log(alunos[1]);
+//console.log(alunos[2]);
+//console.log(alunos[0]);
+//console.log(alunos);
