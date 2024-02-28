1a863c1817545f1fc84be32d0523b86e0e24d368
Michel Machado <mr2710michel@gmail.com>
2024-02-28 10:44:58 -0300

    Feito Aula 21
    Foi explicado todos os Operadores de Comparação. Explicado a maneira que é mais eficaz de utilizar a Igualdade Estrita e Diferença Estrita.

---
+++ aula21/index.js
@@ -0,0 +1,19 @@
+/*
+
+OPERADORES DE COMPARAÇÃO
+
+>    Maior que
+>=   Maior que ou igual a 
+<    Menor que
+<=   Menor que ou igual a
+==   Igualdade (Checa o Valor) *****Não recomendado a utilização*****
+===  Igualdade estrita (Checa o Valor e o Tipo)
+!=   Diferente (Checa o Valor) *****Não recomendado a utilização*****
+!==  Diferente estrito (Checa o Valor e o Tipo)
+
+*/
+
+const num1 = 10;
+const num2 = 10;
+const comp = num1 !== num2;
+console.log(comp);
