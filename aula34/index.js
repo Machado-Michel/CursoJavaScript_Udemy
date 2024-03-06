95392e1fdb961f539c8108aba3da670a2c573263
Michel Machado <mr2710michel@gmail.com>
2024-03-06 14:39:36 -0300

    Feito aula 35
    Visto como funciona a estrutura de repetição For Classica

---
+++ aula34/index.js
@@ -0,0 +1,20 @@
+/*
+console.log('Linha 0');
+console.log('Linha 1');
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+console.log('Linha 5');
+*/
+
+/*
+for (let i = 0; i <= 100; i++) {
+    const par = i % 2 === 0 ? `Par` : `Impar`;
+    console.log(`Linha ${i} = ${par}`);
+} */
+
+const frutas = ['Maça', 'Pêra', 'Uva'];
+
+for (let i = 0; i < frutas.length; i++) {
+console.log(`Indice ${i}`, frutas[i]);
+}
