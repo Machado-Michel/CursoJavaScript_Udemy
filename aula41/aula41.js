0227b6d7ce581dc13a4d5977996f07f323e29ad5
Michel Machado <mr2710michel@gmail.com>
2024-04-02 11:07:10 -0300

    Feito aula 41
    Executado testes de retornar um maior numero. Comecei o exercicio executando IF e Else. Depois, conseegui refatorar o codifo para ficar mais conciso e executado a mesma lógica em uma linha de código, utilizando arrow.

---
+++ aula41/aula41.js
@@ -0,0 +1,10 @@
+/*function maiorNumero (num1, num2) {
+    return num1 > num2 ? num1 : num2;
+} */ // Primeiro modo de executar
+
+/*const maiorNumero2 = (num1, num2) => {
+    return num1 > num2 ? num1 : num2;
+} */  // SEGUNDO modo de Executar
+
+const maiorNumero3 = (num1, num2) => num1 > num2 ? num1 : num2;
+console.log(maiorNumero3(13,15));
