5e1ef834befc19971b64ab675d1966b5e28ab40d
Michel Machado <mr2710michel@gmail.com>
2024-04-10 14:47:34 -0300

    Feito aula 47
    Criado juntamente com o professor, um mini projeto de cronometro digital. Utilizando setInterval, setIntervaloff, clearInterval e manuseando eventos EventListener com 'click'.

---
+++ aula47/assets/js/main.js
@@ -0,0 +1,44 @@
+function relogio() {
+    function criaHoraSegundos(segundos) {
+        const data = new Date(segundos * 1000);
+        return data.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'GMT'
+        });
+    }
+
+    const relogio = document.querySelector('.relogio');
+    let segundos = 0;
+    let timer;
+
+    function iniciaRelogio() {
+        timer = setInterval(function () {
+            segundos++;
+            relogio.innerHTML = criaHoraSegundos(segundos);
+        }, 1000)
+    };
+
+    document.addEventListener('click', function (e) {
+        const el = e.target;
+
+        if (el.classList.contains('iniciar')) {
+            relogio.classList.remove('pausado');
+            clearInterval(timer)
+            iniciaRelogio();
+        }
+
+        if (el.classList.contains('pausar')) {
+            relogio.classList.add('pausado');
+            clearInterval(timer);
+        }
+
+        if (el.classList.contains('zerar')) {
+            relogio.classList.remove('pausado');
+            segundos = 0;
+            clearInterval(timer);
+            relogio.innerHTML = "00:00:00"
+        }
+    })
+
+}
+relogio();
